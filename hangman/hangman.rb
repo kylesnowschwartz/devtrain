42393ad7de60778355d1f7987091e5469d4a83d3
Kyle Snow Schwartz <Kyle.SnowSchwartz@gmail.com>
2015-10-27 13:46:49 +1300

    mucking around with mocks and doubles rspec

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -5,9 +5,9 @@ class Hangman
     raise ArgumentError.new("The number of lives must be greater than zero") unless lives > 0
     raise ArgumentError.new("The word must have 3 or more letters") unless word.chars.count > 2
     @word = parse_word(word)
-    @lives_remaining = lives
-    @guessed_letters = []
     @remaining_letters = parse_word(word)
+    @guessed_letters = []
+    @lives_remaining = lives
     @board = @word.map {|e| "_"}
   end
 
@@ -25,8 +25,10 @@ class Hangman
 
   def take_turn
     if submit_guess(ask_for_letter)
+      clear_screen
       report_success
     else
+      clear_screen
       report_failure
     end
     print_gamestate
@@ -45,7 +47,14 @@ class Hangman
   end
 
   def ask_for_letter
-    player_guess = gets.chomp
+    alphabet = ('A'..'Z').to_a
+    while user_input = gets.upcase.chomp
+      if alphabet.include?(user_input)
+        return user_input
+      else
+        puts "Please select a single letter in the English alphabet:"
+      end
+    end
   end
 
   def report_success
@@ -57,7 +66,6 @@ class Hangman
   end
 
   def print_gamestate
-    clear_screen
     puts "Thusfar, you've guessed:"
     puts @guessed_letters.uniq.join(" ")
     puts "You have #{@remaining_letters.length} letters remaining"
@@ -96,7 +104,7 @@ class Hangman
   end
 
   def parse_word(word)
-    word.chars
+    word.upcase.chars
   end
 
   def subtract_life
--- hangman/spec/hangman_spec.rb
+++ hangman/spec/hangman_spec.rb
@@ -1,14 +1,15 @@
 require './hangman.rb'
 
 RSpec.describe Hangman do
+	before do
+		@hangman = Hangman.new(8, "bottle")
+	end
+
 	describe "#initialize" do
-		before do
-			@hangman = Hangman.new(8, "bottle")
-		end
 
 		context "with a correct arguments" do
-			it "sets #word to an array of letters" do
-				expect(@hangman.word).to eq(["b", "o", "t", "t", "l", "e"]) 
+			it "sets #word to an array of uppercase letters" do
+				expect(@hangman.word).to eq(["B", "O", "T", "T", "L", "E"]) 
 			end
 
 			it "sets #lives_remaining to the correct number of lives" do
@@ -20,7 +21,7 @@ RSpec.describe Hangman do
 			end
 
 			it "sets #remaining_letters to an array of letters" do
-				expect(@hangman.remaining_letters).to eq(["b", "o", "t", "t", "l", "e"])
+				expect(@hangman.remaining_letters).to eq(["B", "O", "T", "T", "L", "E"])
 			end
 
 			it "sets #board to an array of underscores" do
@@ -40,6 +41,22 @@ RSpec.describe Hangman do
 		end
 	end
 
+	describe "#play" do
+		it "takes turns equal to at least the amount of letters of the word" do			
+			
+		end		
 
-	
+		it "takes turns equal to at least the amount of letters of the word" do			
+			game = instance_double("Hangman", :word => ["B", "O", "T", "T", "L", "E"],
+																		:remaining_letters => ["B", "O", "T", "T", "L", "E"],
+																		:guessed_letters => [],
+																		:lives_remaining => 8,
+																		:board => ["_", "_", "_", "_", "_", "_"]
+														)
+			allow(game).to receive(:ask_for_letter).and_return("B", "O", "T", "T", "L", "E")
+			allow(game).to receive(:submit_guess).with(:ask_for_letter).and_return(true)
+			allow(game).to receive(:play)
+			expect(game).to receive(:take_turn).at_least(5).times
+		end
+	end
 end
