7c64dca224d1f979f3e99b5d5dd1f91e42924ea7
Kyle Snow Schwartz <Kyle.SnowSchwartz@gmail.com>
2015-10-30 15:51:20 +1300

    added begin game spec

--- spec/gameview_spec.rb
+++ spec/gameview_spec.rb
@@ -3,6 +3,7 @@ require_relative '../lib/gameview.rb'
 RSpec.describe GameView do
 	let(:input) { double("input") }
 	let(:output) { double("output") }
+	subject(:state) { instance_double("GameState") }
 	subject(:view) { GameView.new(input, output) }
 
 	describe "#ask_for_letter" do
@@ -35,6 +36,18 @@ RSpec.describe GameView do
 				expect(view).to receive(:report_invalid_input).exactly(5).times
 				expect(view.ask_for_letter).to eq "B"
 			end
+		end
+	end
+
+	describe "#begin_game" do
+		before do
+			expect(output).to receive(:puts).with("Welcome to hangman. Guess a letter.")
+			expect(state).to receive(:board).and_return(["_"])
+		end
+
+		it "should print the board" do
+			expect(output).to receive(:puts).with("_")
+			view.begin_game(state)
 		end
 	end
 end
